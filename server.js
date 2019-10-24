d2d79001c7ba7b528addeba6a82da8d7a194c0c5
kesh24 <keshnarana4@gmail.com>
2019-10-23 21:40:14 -0400

    scrape

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(routes);
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 
 
